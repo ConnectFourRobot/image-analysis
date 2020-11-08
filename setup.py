1fa15f0692cb61782edef3fa780f4ce8851d7e4a
Simple-codinger <vincentgierisch@gmail.com>
2020-11-08 02:11:11 +0100

    add needed files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='vgr-image-analysis',
+    version='0.1',
+    description='Analyzes the board'
+    author='Vincent Gierisch',
+    author_email='vincent.gierisch@st.oth-regensburg.de'
+)
