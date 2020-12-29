373501902f3b8c851785fb9b64fb1089ad62337e
Simple-codinger <vincentgierisch@gmail.com>
2020-12-29 02:13:44 +0100

    improve dev-tools

--- dev-tools/run.sh
+++ dev-tools/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cat $(dirname "$BASH_SOURCE")/asciiArt.txt
+
+if [[ "$VIRTUAL_ENV" != "" ]] 
+then
+    echo "This script runs in an active virtual environment. This is fine for me. But is it fine for you?"
+    INVENV=1
+else
+    echo "This script doesn't run in an virtual environment. This is fine. I will start it for you."
+    INVENV=0
+fi
+
 if [ ! -x "$(command -v python3.8)" ]; then
     echo "Python3.8 is not installed. Please install it manually and run this script again." >&2
     exit 1
@@ -7,9 +18,14 @@ fi
 
 cd $(dirname "$BASH_SOURCE")/..
 
-echo "Run virtual environment..."
-source venv/bin/activate
+if [ "$INVENV" = 0 ]; then
+    echo "Run virtual environment..."
+    source venv/bin/activate
+fi
 
 python -m image_analyzer 127.0.0.1 7777
 
-deactivate
+if [ "$INVENV" = 0 ]; then
+    echo "Stop virtual environment..."
+    deactivate
+fi
